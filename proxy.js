f39218de0ecfedcb62f34674e0892ca15914ea1b
mamedul2 <md.mamedulislam+2@gmail.com>
2025-08-13 02:57:18 +0600

    3rd update

--- proxy.js
+++ proxy.js
@@ -12,16 +12,41 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/:protocol(http|https)/*', (req, res, next) => {
-  const targetUrl = `${req.params.protocol}://${req.params[0]}`;
-  const proxy = createProxyMiddleware({
-    target: targetUrl,
-    changeOrigin: true,
-    secure: false,
-    logLevel: 'silent',
+try{
+  app.use('/:protocol(http|https)/*', (req, res, next) => {
+    const targetUrl = `${req.params.protocol}://${req.params[0]}`;
+    const proxy = createProxyMiddleware({
+      target: targetUrl,
+      changeOrigin: true,
+      secure: false,
+      logLevel: 'silent',
+    });
+    proxy(req, res, next);
   });
-  proxy(req, res, next);
-});
+
+}catch(err){
+  app.use('/*', (req, res, next) => {
+    const fullPath = req.params[0]; // everything after the first slash
+    let targetUrl;
+
+    try {
+      targetUrl = decodeURIComponent(fullPath);
+    } catch {
+      return res.status(400).send('Invalid target URL encoding');
+    }
+
+    if (!/^https?:\/\//i.test(targetUrl)) {
+      return res.status(400).send('Invalid target URL. Must start with http:// or https://');
+    }
+
+    createProxyMiddleware({
+      target: targetUrl,
+      changeOrigin: true,
+      secure: false,
+      logLevel: 'silent',
+    })(req, res, next);
+  });
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
