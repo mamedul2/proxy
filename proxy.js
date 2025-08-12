59b6a2fae2383ab8e3d6d5bc295e65c4b942e051
mamedul2 <md.mamedulislam+2@gmail.com>
2025-08-13 03:44:02 +0600

    final commit

--- proxy.js
+++ proxy.js
@@ -1,50 +1,37 @@
-//require('dotenv').config();
+require('dotenv').config({ quiet: true });
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
 app.use((req, res, next) => {
+
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,PATCH,OPTIONS,HEAD');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
   if (req.method === 'OPTIONS') return res.sendStatus(204);
-  next();
+
+  const fullPath = req.path.slice(1);
+  let targetUrl;
+  
+  try {
+    targetUrl = decodeURIComponent(fullPath);
+  } catch {
+    return res.status(400).send('Invalid URL encoding');
+  }
+
+  if (!/^(http|https)?:\/\//i.test(targetUrl)) {
+    return res.status(400).send('Invalid target URL. *Must start with http:// or https://');
+  }
+
+  createProxyMiddleware({
+    target: targetUrl,
+    changeOrigin: true,
+    secure: false,
+    logLevel: 'silent',
+  })(req, res, next);
+
 });
-
-try {
-  app.use('/:protocol(http|https)://:targetUrl(*)', (req, res, next) => {
-    const targetUrl = `${req.params.protocol}://${req.params.targetUrl}`;
-    const proxy = createProxyMiddleware({
-      target: targetUrl,
-      changeOrigin: true,
-      secure: false,
-      logLevel: 'silent',
-    });
-    proxy(req, res, next);
-  });
-} catch (err) {
-  app.all('*', (req, res, next) => {
-    const fullPath = req.path.slice(1);
-    let targetUrl;
-    try {
-      targetUrl = decodeURIComponent(fullPath);
-    } catch {
-      return res.status(400).send('Invalid URL encoding');
-    }
-
-    if (!/^https?:\/\//i.test(targetUrl)) {
-      return res.status(400).send('Invalid target URL. Must start with http:// or https://');
-    }
-
-    createProxyMiddleware({
-      target: targetUrl,
-      changeOrigin: true,
-      secure: false,
-      logLevel: 'silent',
-    })(req, res, next);
-  });
-}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
